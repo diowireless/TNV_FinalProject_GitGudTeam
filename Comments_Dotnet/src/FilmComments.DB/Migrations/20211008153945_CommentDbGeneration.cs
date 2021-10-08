2e659904c18145841b2d90776d6029635b821390
GIANMARCO ADDARI <gianmarco.addari@gmail.com>
2021-10-08 17:49:00 +0200

    Adding Dotnet project

---
+++ Comments_Dotnet/src/FilmComments.DB/Migrations/20211008153945_CommentDbGeneration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FilmComments.DB.Migrations
+{
+    public partial class CommentDbGeneration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "comments",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    user_id = table.Column<int>(type: "int", nullable: false),
+                    movie_id = table.Column<int>(type: "int", nullable: false),
+                    comment = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_comments", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "comments");
+        }
+    }
+}
