713280f0dac60898394e61428141800947cb19a2
Gianfranco Manca <90779383+GianfrancoMan@users.noreply.github.com>
2021-10-10 14:45:18 +0200

    Add files via upload

---
+++ subTutorial1/src/main/java/com/thenetvalue/sbTutorial1/SubTutorial1Application.java
@@ -0,0 +1,13 @@
+package com.thenetvalue.sbTutorial1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubTutorial1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubTutorial1Application.class, args);
+	}
+
+}
