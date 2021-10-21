622030c3542d9dfcecb85c79d31051411d851a31
GIANMARCO ADDARI <gianmarco.addari@gmail.com>
2021-10-21 12:13:26 +0200

    Changed welcome page. logged user goes to pick-location, not logged user goes to login

--- MainApp_Angular/Frontend/src/app/routes/welcome-page/welcome-page.component.ts
+++ MainApp_Angular/Frontend/src/app/routes/welcome-page/welcome-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TransporterService } from '../../services/transporter.service';
+import { UserData } from '../../models/UserData';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -8,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomePageComponent implements OnInit {
 
-  constructor() { }
+  user : UserData;
+
+  constructor(private transporterService : TransporterService,
+              private router : Router) { }
 
   ngOnInit(): void {
+    this.user = this.transporterService.userTransported;
+  }
+
+  gotoMainPage() {
+    if(this.user)
+      this.router.navigate(['/pick-location'])
+    else
+      this.router.navigate(['/login'])
   }
 
 }
